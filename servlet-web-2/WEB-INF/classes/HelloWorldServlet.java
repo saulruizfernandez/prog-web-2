5a8303fdbf2c00d229977d726cfef50156a807a4
saulruizfernandez <saul.ruiz.30@ull.edu.es>
2025-06-02 13:22:16 +0200

    servlet configured

---
+++ servlet-web-2/WEB-INF/classes/HelloWorldServlet.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class HelloWorldServlet extends HttpServlet {
+  public void doGet(HttpServletRequest request,
+      HttpServletResponse response)
+      throws IOException, ServletException {
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    out.println(
+        "<html><head><title>Hello World Servlet</title></head>");
+    out.println("<body>");
+    out.println("<h1>Hello World!</h1>");
+    out.println("</body></html>");
+    out.close();
+  }
+}

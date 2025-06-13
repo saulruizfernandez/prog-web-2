7bc7470bb447f974f2349d635931270c7d723f4f
saulruizfernandez <saul.ruiz.30@ull.edu.es>
2025-06-13 11:29:07 +0200

    end

--- myapp/progweb2/views.py
+++ myapp/progweb2/views.py
@@ -8,10 +8,10 @@ def local_server(request):
     if request.method != "POST":
         return JsonResponse({"error": "Only POST method is allowed."}, status=405)
     try:
-        payload = json.loads(request.body)
+        payload = json.loads(request.body) # Extract json from the body of the POST request
         for table_name, records in payload.items():
-            collection = db[table_name]
-            result = collection.insert_many(records)
+            collection = db[table_name] # Create a collection for each table
+            result = collection.insert_many(records) # Insert records into the collection
         return JsonResponse({"status": "success"}, status=201)
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=400)
--- php_server/index.php
+++ php_server/index.php
@@ -4,14 +4,17 @@ $username = "arsapi";
 $password = "";
 $dbname = "my_arsapi";
 
+// Connection with mySQL database
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $table_names = [];
   $stmt = $conn->query("SHOW TABLES");
+  // Store all the table names from the SQL database
   while ($tN = $stmt->fetch(PDO::FETCH_NUM)) {
     $table_names[] = $tN[0];
   }
+  // Select every attribute and every instance from each table
   foreach ($table_names as $table_name) {
     $data_for_table = [];
     $stmt = $conn->query("SELECT * FROM `$table_name`");
@@ -20,6 +23,7 @@ try {
     }
     $database_json[$table_name] = $data_for_table;
   }
+  // Encode in json in human-readable format (debuging)
   $json_output = json_encode($database_json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
   header('Content-Type: application/json');
   echo $json_output; // HTTP response
